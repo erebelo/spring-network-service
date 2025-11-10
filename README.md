cbb526c48d171fbc757d1ab50b28f1381c9ca35b
erebelo <eduldrebelo@gmail.com>
2025-11-10 15:39:14 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Network Service
+
+REST API project developed in Java using Spring Boot 3 and MongoDB to manage and hydrate complex network relationships between business contracts.
---
+++ src/main/java/com/erebelo/springnetworkservice/SpringNetworkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.erebelo.springnetworkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNetworkServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNetworkServiceApplication.class, args);
+    }
+
+}
